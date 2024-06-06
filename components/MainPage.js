6f34c8e1f1fef165b40c75181f9415aa225702a1
alindor185 <alindor185@gmail.com>
2024-06-06 13:14:59 +0300

    Chnage in style of writing - working .

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import '../App.css'; // Ensure this path is correct
-import youtubeLogo from '../assets/youtube-logo.png'; // Ensure this path is correct
+import '../App.css'; 
+import youtubeLogo from '../assets/youtube-logo.png'; 
 
 function MainPage({ setPage }) {
   return (
     <div className="main-page">
       <div className="form-container">
-        {/* YouTube Logo */}
         <img src={youtubeLogo} alt="YouTube Logo" className="logo" />
         <h1>Welcome Back!</h1>
         <p>To keep connected with us please login with your personal info</p>
         <form>
           <label>
             Username
-            <input type="text" placeholder="Enter username" aria-label="Username" />
+            <input type="text" placeholder="Enter username" />
           </label>
           <label>
             Password
-            <input type="password" placeholder="Password" aria-label="Password" />
+            <input type="password" placeholder="Password" />
           </label>
-          <div className="error-message">
-            {/* Error messages will be displayed here */}
-          </div>
           <button type="button" className="sign-in-btn" onClick={() => setPage('login')}>Sign In</button>
         </form>
         <span>or continue with</span>
@@ -36,9 +31,5 @@ function MainPage({ setPage }) {
     </div>
   );
 }
-
-MainPage.propTypes = {
-  setPage: PropTypes.func.isRequired,
-};
 
 export default MainPage;
